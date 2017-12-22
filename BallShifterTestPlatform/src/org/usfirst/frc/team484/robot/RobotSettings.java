c10c9ad8cfa7a3da6c9bc1559c4e859082670e25
Kevin <keco185@me.com>
2017-12-22 08:58:24 -0500

    Update RobotSettings.java
    Updates the DPP

--- BallShifterTestPlatform/src/org/usfirst/frc/team484/robot/RobotSettings.java
+++ BallShifterTestPlatform/src/org/usfirst/frc/team484/robot/RobotSettings.java
@@ -6,8 +6,8 @@ package org.usfirst.frc.team484.robot;
  * (Basically anything that isn't a physical port on the robot)
  */
 public class RobotSettings {
-	public static final double LEFT_ENCODER_DISTANCE_PER_PULSE = -0.111264646;
-	public static final double RIGHT_ENCODER_DISTANCE_PER_PULSE = 0.111264646;
+	public static final double LEFT_ENCODER_DISTANCE_PER_PULSE = -0.03337939375;
+	public static final double RIGHT_ENCODER_DISTANCE_PER_PULSE = 0.03337939375;
 	public static final double LOW_GEAR_ROTATIONS_PER_ENCODER_PULSE = 10.42/256.0/3.0;
 	public static final double HIGH_GEAR_ROTATIONS_PER_ENCODER_PULSE = 2.83/256.0/3.0;
 	
